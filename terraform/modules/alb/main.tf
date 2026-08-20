d224004fd62200271ca0e5dcff54c6e271e8d6d1
unknown <utkarshsonawane534@gmail.com>
2026-08-20 11:14:37 +0530

    modification

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -11,14 +11,6 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "Allow HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -56,10 +48,8 @@ resource "aws_lb_target_group" "tenant_tg" {
 
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.test.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
     type             = "forward"
